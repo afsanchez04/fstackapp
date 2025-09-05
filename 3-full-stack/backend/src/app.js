73885dfb11b7bdd6a072134a0bbca2406dc9d567
afsaanchez04 <afsanchez04@gmail.com>
2025-09-04 23:50:25 -0500

    actualizacion rutas

--- 3-full-stack/backend/src/app.js
+++ 3-full-stack/backend/src/app.js
@@ -13,7 +13,7 @@ const app = express()
 
 app.use( express.json() )
 app.use( cors({
-  origin: "https://1-web-d-proyecto-final-83lg.vercel.app/",
+  origin: "http://localhost:5173/",
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }) )
