511cd3a0ab51e61634e6566efda0380edd9e3f65
afsaanchez04 <afsanchez04@gmail.com>
2025-09-05 00:23:04 -0500

    nuevo origin

--- 3-full-stack/backend/src/app.js
+++ 3-full-stack/backend/src/app.js
@@ -15,7 +15,8 @@ app.use( express.json() )
 app.use( cors({
   origin: [
     "http://localhost:5173",
-    "https://fstackapp-69zl.vercel.app"
+    "https://fstackapp-69zl.vercel.app",
+    "https://fstackapp-69zl.vercel.app/"
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
