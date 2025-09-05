d1388735b2adcb13a3a91bea6ba1fe1f7e52f7f8
afsaanchez04 <afsanchez04@gmail.com>
2025-09-05 00:13:07 -0500

    add new origin

--- 3-full-stack/backend/src/app.js
+++ 3-full-stack/backend/src/app.js
@@ -13,7 +13,10 @@ const app = express()
 
 app.use( express.json() )
 app.use( cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://fstackapp-69zl.vercel.app"
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }) )
